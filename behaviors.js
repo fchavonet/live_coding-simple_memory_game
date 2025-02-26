6dfc6932bce0ecd2604394c45174e5369ce2a327
fchavonet <fchavonet@mailo.com>
2025-02-26 16:01:32 +0100

    Creating "createCard" function.

--- behaviors.js
+++ behaviors.js
@@ -1,5 +1,31 @@
-const card = document.querySelector(".card");
+const gameContainer = document.getElementById("game-container");
 
-card.addEventListener("click", function () {
-	card.classList.add("flipped");
+// Create a card
+function createCard() {
+	const card = document.createElement("div");
+	card.classList.add("card");
+
+	const cardInner = document.createElement("div");
+	cardInner.classList.add("card-inner");
+
+	const cardFront = document.createElement("div");
+	cardFront.classList.add("card-front");
+
+	const cardBack = document.createElement("div");
+	cardBack.classList.add("card-back");
+
+	cardInner.appendChild(cardFront);
+	cardInner.appendChild(cardBack);
+
+	card.appendChild(cardInner);
+
+	gameContainer.appendChild(card);
+
+	card.addEventListener("click", function () {
+		card.classList.add("flipped");
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	createCard();
 });
